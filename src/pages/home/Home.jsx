e7cb508bb7d281cce34c51df2fe81d59d532d379
alpertanrikulu <tanrikulualper77100@gmail.com>
2025-01-19 06:56:00 +0300

    commit 1

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div>
+      Home -- Bootstrap and Sass is activated if the button below is orange
+      <div>
+        <button className="btn btn-orange">Button</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
